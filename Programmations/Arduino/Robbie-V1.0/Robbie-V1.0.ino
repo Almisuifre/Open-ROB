6cfe073d9209f03cc4e7f7e43880aa03f640fa67
Almisuifre <alexandre.labure@gmail.com>
2015-12-27 09:57:02 +0100

    Mise à jour du programme de test

--- Programmations/Arduino/Robbie-V1.0/Robbie-V1.0.ino
+++ Programmations/Arduino/Robbie-V1.0/Robbie-V1.0.ino
@@ -6,6 +6,12 @@
 Move moteurs(dirMotDroit, stepMotDroit, dirMotGauche, stepMotGauche); //Classe moteur
 MaxSonar sonar(capteurUs);  //Classe sonar
 
+/* -- VARIABLES -- */
+long capteurSonar = 0;
+int valAction = 1;
+int distance = 0;
+
+/* -- SETUP -- */
 void setup() {
   Serial.begin(9600);
   
@@ -21,11 +27,64 @@ void setup() {
   moteurs.reverse(true, false);
 }
 
+
+/* -- LOOP -- */
 void loop() {
+  
+  //Tester le capteur US
+  capteurSonar = sonar.measureCm();
+
+  //Serial.println(capteurSonar);
+  
+  //Si la distance est de 25 cm
+  if(capteurSonar <= 70) {
+
+    //Créer un nombre aléatoire
+    valAction = random(1,(6+1));
+
+    digitalWrite(led, HIGH);
+
+    //En fonction du nombre obtenu
+    switch(valAction) {
+      case 1:
+        if(capteurSonar < 25) { break; }
+        distance = random(200, (1000+1));
+        moteurs.action(distance, 1, 1); //Aller en avant
+        break;
+      case 2:
+        distance = random(200, (1000+1));
+        moteurs.action(distance, -1, -1); //Aller en arrière
+        break;
+      case 3:
+        moteurs.action(1000, 1, -1); //Aller gauche de 1000 pas 90°
+        break;
+      case 4:
+        moteurs.action(1000, -1, 1); //Aller gauche de 1000 pas 90°
+        break;
+      case 5:
+        distance = random(200, (1000+1));
+        moteurs.action(distance, 1, 0); //Aller à gauche
+        break;
+      case 6:
+        distance = random(200, (1000+1));
+        moteurs.action(distance, 0, 1); //Aller à droite
+        break;
+    }
+    digitalWrite(led, LOW);
+  }
+  else {
+    moteurs.action(1000, 1, 1); //Aller en avant
+  }
+  
+  delay(10);
+  /*
+  varDom = random(1,6);
+
+  Serial.println(varDom);
+
+  delay(1000);
 
   long toto = sonar.measureCm();
-
-  //Serial.println(toto);             // debug value
 
   digitalWrite(led, HIGH);
   
@@ -34,7 +93,7 @@ void loop() {
   digitalWrite(led, LOW);
 
   delay(toto*2);
-  /*
+  
   //Phase 1
   moteurs.action(1000, 1, 1); //Aller en avant de 1000 pas
   
